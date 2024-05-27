abed29720fe9fbbb73fee6a8d47e9290563deb73
TofaelKabir <tofaelkabir@gmail.com>
2024-05-26 21:05:04 -0400

    Inheritance, Abstraction

---
+++ src/lec07_01_java_oop_inheritance/FamilyHierarchy.java
@@ -0,0 +1,57 @@
+package lec07_01_java_oop_inheritance;
+
+public class FamilyHierarchy {
+
+	public static void main(String[] args) {
+		GrandFather grandFather = new GrandFather();
+		grandFather.grandFatherInfo();		
+		
+		System.out.println("\n----------------------------------------------------");
+		Aunt aunt = new Aunt();
+		aunt.auntInfo();
+		aunt.grandFatherInfo();
+		
+		System.out.println("\n----------------------------------------------------");
+		Cousin cousin = new Cousin();
+		cousin.cousinInfo();
+		cousin.auntInfo();
+		cousin.grandFatherInfo();
+		
+		System.out.println("\n----------------------------------------------------");
+		Uncle uncle = new Uncle();
+		uncle.uncleInfo();
+		uncle.grandFatherInfo();
+		
+		System.out.println("\n----------------------------------------------------");
+		Father father = new Father();
+		father.fatherInfo();
+		father.grandFatherInfo();
+		
+		System.out.println("\n----------------------------------------------------");
+		MySelf mySelf = new MySelf();
+		mySelf.myInfo();
+		mySelf.fatherInfo();
+		mySelf.grandFatherInfo();
+		
+		System.out.println("\n----------------------------------------------------");
+		Sister sister = new Sister();
+		sister.sisterInfo();
+		sister.fatherInfo();
+		sister.grandFatherInfo();
+		
+		System.out.println("\n----------------------------------------------------");
+		Niece niece = new Niece();
+		niece.nieceInfo();
+		niece.sisterInfo();
+		niece.fatherInfo();
+		niece.grandFatherInfo();
+		
+		// Niece extends Sister, Sister extends Father, Father extends GrandFather
+		// Example of Multilevel Inheritance
+		
+		// Aunt extends GrandFather, Father extends GrandFather, Uncle extends GrandFather
+		// Example of Hierarchical Inheritance
+
+	}
+
+}
---
+++ src/lec07_01_java_oop_inheritance/GrandFather.java
@@ -0,0 +1,14 @@
+package lec07_01_java_oop_inheritance;
+
+public class GrandFather {
+	// Constructor
+	public GrandFather() {
+		System.out.println("------------: I am a Constructor from GrandFather class :----------");
+	}
+	
+	// void type method
+	public void grandFatherInfo() {
+		System.out.println("GrandFather: Ismail");
+	}
+
+}
